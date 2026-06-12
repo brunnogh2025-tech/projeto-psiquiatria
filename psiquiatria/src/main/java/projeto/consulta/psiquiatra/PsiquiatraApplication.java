4857027a2441b0c5345ddbaf0d9cd635d328b3f4
Brunno dos Santos Garcia <brunnogh2025@gmail.com>
2026-06-12 16:59:15 -0300

    maven configurado

---
+++ psiquiatria/src/main/java/projeto/consulta/psiquiatra/PsiquiatraApplication.java
@@ -0,0 +1,13 @@
+package projeto.consulta.psiquiatra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PsiquiatraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PsiquiatraApplication.class, args);
+	}
+
+}
---
+++ psiquiatria/src/test/java/projeto/consulta/psiquiatra/PsiquiatraApplicationTests.java
@@ -0,0 +1,13 @@
+package projeto.consulta.psiquiatra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PsiquiatraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
