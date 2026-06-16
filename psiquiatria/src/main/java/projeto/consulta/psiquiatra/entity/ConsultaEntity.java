1ebfc84ec1d685d8a6a15a15ce28342de7bb2e90
Brunno dos Santos Garcia <brunnogh2025@gmail.com>
2026-06-16 17:59:56 -0300

    Criei a entidade de consulta

---
+++ psiquiatria/src/main/java/projeto/consulta/psiquiatra/entity/ConsultaEntity.java
@@ -0,0 +1,23 @@
+package projeto.consulta.psiquiatra.entity;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "consulta")
+public class ConsultaEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "consulta_id")
+    private Long id;
+
+    @ManyToOne()
+    private PacienteEntity paciente_id;
+
+    private String paciente_nome;
+
+    @JsonFormat(pattern = "dd/MM/yyyy HH:mm")
+    private LocalDateTime dataHora;
+}
