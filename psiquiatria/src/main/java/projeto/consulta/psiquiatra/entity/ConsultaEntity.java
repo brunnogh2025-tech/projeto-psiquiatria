3dc05da916819ca606c238c74fef70965bf52644
Brunno dos Santos Garcia <brunnogh2025@gmail.com>
2026-06-17 17:17:07 -0300

    Foram adicionados os métodos especiais

--- psiquiatria/src/main/java/projeto/consulta/psiquiatra/entity/ConsultaEntity.java
+++ psiquiatria/src/main/java/projeto/consulta/psiquiatra/entity/ConsultaEntity.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.*;
 
 import java.time.LocalDateTime;
+import java.util.Objects;
 
 @Entity
 @Table(name = "consulta")
@@ -25,4 +26,75 @@ public class ConsultaEntity {
 
     @JsonFormat(pattern = "dd/MM/yyyy HH:mm")
     private LocalDateTime dataHora;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public PacienteEntity getPaciente_id() {
+        return paciente_id;
+    }
+
+    public void setPaciente_id(PacienteEntity paciente_id) {
+        this.paciente_id = paciente_id;
+    }
+
+    public String getPaciente_nome() {
+        return paciente_nome;
+    }
+
+    public void setPaciente_nome(String paciente_nome) {
+        this.paciente_nome = paciente_nome;
+    }
+
+    public PsiquiatraEntity getPsiquiatra_id() {
+        return psiquiatra_id;
+    }
+
+    public void setPsiquiatra_id(PsiquiatraEntity psiquiatra_id) {
+        this.psiquiatra_id = psiquiatra_id;
+    }
+
+    public String getPsiquiatra_nome() {
+        return psiquiatra_nome;
+    }
+
+    public void setPsiquiatra_nome(String psiquiatra_nome) {
+        this.psiquiatra_nome = psiquiatra_nome;
+    }
+
+    public LocalDateTime getDataHora() {
+        return dataHora;
+    }
+
+    public void setDataHora(LocalDateTime dataHora) {
+        this.dataHora = dataHora;
+    }
+
+    public ConsultaEntity(PacienteEntity paciente_id, String paciente_nome, PsiquiatraEntity psiquiatra_id, String psiquiatra_nome, LocalDateTime dataHora) {
+        this.paciente_id = paciente_id;
+        this.paciente_nome = paciente_nome;
+        this.psiquiatra_id = psiquiatra_id;
+        this.psiquiatra_nome = psiquiatra_nome;
+        this.dataHora = dataHora;
+    }
+
+    public ConsultaEntity() {
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        ConsultaEntity that = (ConsultaEntity) o;
+        return Objects.equals(paciente_nome, that.paciente_nome) && Objects.equals(psiquiatra_nome, that.psiquiatra_nome) && Objects.equals(dataHora, that.dataHora);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(paciente_nome, psiquiatra_nome, dataHora);
+    }
 }
--- psiquiatria/src/main/java/projeto/consulta/psiquiatra/entity/PacienteEntity.java
+++ psiquiatria/src/main/java/projeto/consulta/psiquiatra/entity/PacienteEntity.java
@@ -23,4 +23,44 @@ public class PacienteEntity {
     @NotBlank
     private byte idade;
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public byte getIdade() {
+        return idade;
+    }
+
+    public void setIdade(byte idade) {
+        this.idade = idade;
+    }
+
+    public PacienteEntity(String telefone, String email, byte idade) {
+        this.telefone = telefone;
+        this.email = email;
+        this.idade = idade;
+    }
+
+    public PacienteEntity() {
+    }
 }
